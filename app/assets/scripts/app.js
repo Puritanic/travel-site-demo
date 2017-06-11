101c67eed2aacd5a7671cc9a429714d30159f100
Puritanic <darktasevski@gmail.com>
2017-06-11 21:10:44 +0200

    Finished Modal config

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -3,8 +3,10 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/scrollReveal';
 import $ from 'jquery';
 import FixedHeader from './modules/FixedHeader';
+import Modal from './modules/Modal';
 
 const mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonial'), '75%');
 const fixedHeader = new FixedHeader();
+const modal = new Modal();
