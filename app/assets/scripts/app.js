c93cea62c90a70831f7a50ae507fc9ce505089d2
Puritanic <darktasevski@gmail.com>
2017-06-11 18:16:21 +0200

    Finished header fixing

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,8 +1,10 @@
 
 import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/scrollReveal.js';
+import RevealOnScroll from './modules/scrollReveal';
 import $ from 'jquery';
+import FixedHeader from './modules/FixedHeader';
 
 const mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonial'), '75%');
+const fixedHeader = new FixedHeader();
