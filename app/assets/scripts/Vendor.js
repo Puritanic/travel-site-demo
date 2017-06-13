50e711efd6175b37dab9edfa1703061afa84dafa
Puritanic <darktasevski@gmail.com>
2017-06-13 11:50:49 +0200

    added support for legacy browsers

--- app/assets/scripts/Vendor.js
+++ app/assets/scripts/Vendor.js
@@ -1,1 +1,3 @@
-import 'lazysizes';
+import '../../temp/scripts/modernizr';
+import 'picturefill'; // responsive picture support for legacy browsers
+import 'lazysizes'; // picture optimisation, so that page can load faster
